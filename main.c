1363ab5d8ce5992760d4b76544a1fc105ea32718
jamesstocktonj1 <james.stocktonj@gmail.com>
2022-02-23 21:23:17 +0000

    Update main.c

--- main.c
+++ main.c
@@ -19,8 +19,9 @@
 
 
 //screen functions
-void draw_bar(uint16_t value, uint8_t colour);
-
+void draw_bar(uint16_t value, uint16_t colour);
+void draw_battery_state(battery state);
+void draw_indicator(uint8_t state);
 
 
 void init_timers(void);
@@ -59,6 +60,9 @@ int main() {
     refreshArea.bottom = LCDHEIGHT / 2;
     refreshArea.right = LCDWIDTH;
     
+    uint8_t state = 0;
+
+    battery bat_state = CHARGING;
 
     while(1) {
 
@@ -101,6 +105,44 @@ int main() {
                 display.y += 10;
                 display.x = 0;
             }
+
+            for(i=0; i<4; i++) {
+
+                char num[1];
+                itoa(i, num, 10);
+
+                display_string("Indicator ");
+                display_string(num);
+                display_string(": ");
+
+                display.x = 100;
+                draw_indicator(state);
+
+                display.y += 10;
+                display.x = 0;
+            }
+
+            display_string("Battery State:");
+            display.x = 100;
+
+            draw_battery_state(bat_state);
+        }
+
+        state = !state;
+
+        switch(bat_state) {
+
+            case CHARGING:
+                bat_state = DISCHARGING;
+                break;
+
+            case DISCHARGING:
+                bat_state = DISCONNECTED;
+                break;
+
+            default:
+                bat_state = CHARGING;
+                break;
         }
 
         _delay_ms(500);
@@ -119,7 +161,7 @@ void init_timers() {
 
 
 
-void draw_bar(uint16_t value, uint8_t colour) {
+void draw_bar(uint16_t value, uint16_t colour) {
 
     //convert 0-1024 to 0-128
     value = value / 4;
@@ -132,4 +174,51 @@ void draw_bar(uint16_t value, uint8_t colour) {
     bar.right = display.x + value;
     
     fill_rectangle(bar, colour);
+}
+
+void draw_battery_state(battery state) {
+
+    //create indicator square of size 7x7 (pixel height square)
+    rectangle ind;
+    ind.top = display.y;
+    ind.left = display.x;
+    ind.bottom = display.y + 7;
+    ind.right = display.x + 7;
+
+    //color variable
+    uint16_t col;
+
+    switch(state) {
+
+        case CHARGING:
+            col = GREEN;
+            break;
+
+        case DISCHARGING:
+            col = RED;
+            break;
+
+        default:
+            col = YELLOW;
+            break;
+    }
+
+    //draw indicator
+    fill_rectangle(ind, col);
+}
+
+void draw_indicator(uint8_t state) {
+
+    //create indicator square of size 7x7 (pixel height square)
+    rectangle ind;
+    ind.top = display.y;
+    ind.left = display.x;
+    ind.bottom = display.y + 7;
+    ind.right = display.x + 7;
+
+    //color variable (green or read)
+    uint16_t col = (state) ? GREEN : RED;
+
+    //draw indicator
+    fill_rectangle(ind, col);
 }
