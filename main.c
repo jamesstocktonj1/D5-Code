7b628d1d57c8536581214bab2bfc875aa6cca2f7
jamesstocktonj1 <james.stocktonj@gmail.com>
2022-03-10 16:47:43 +0000

    algorithm in main

--- main.c
+++ main.c
@@ -66,17 +66,19 @@ void algorithm(void);
 
 //main global values
 //analog input values
-uint16_t mains_capacity;
 uint16_t busbar_voltage, busbar_current;
-uint16_t wind_capacity, solar_capacity;
+float wind_capacity, solar_capacity;
 
 //digital input values
 uint8_t load1_call, load2_call, load3_call;
 
+//Analogue output values
+uint16_t mains_capacity;
+
 //digital output values
 uint8_t load1, load2, load3;
 battery battery_state = DISCONNECTED;
-uint8_t battery_charge;
+int8_t battery_charge;
 
 
 
@@ -156,6 +158,135 @@ int main() {
     return 1;
 }
 
+// Main algorithm to be implemented in project
+// Average score of 79.16%
+int algorithm(struct test a, int i)
+{
+    double hour = millis_timer/(60*1000);
+
+    // Set constants to zero
+    int housesLost = 0;
+    float MainsReq = 0;
+
+    // Calculate power necessary
+    float PowerRequired = 1.2*load1_call + 2*load2_call + 0.8*load3_call;
+    float PowerDeficit = PowerRequired - wind_capacity - solar_capacity;
+
+    float mainsSupply = (V2_2 - V2_5) - a.Wind[i] - a.PV[i];
+
+    // Charge battery if excess renewable
+    if (PowerDeficit < -1) 
+    {
+        battery_state = CHARGING;
+        MainsReq = 0;
+
+    }
+
+    // Charge battery if some spare renewable and make up the difference with mains
+    // Change to -0.1 increases score but is probably specific to these tests
+    else if (-1 <= PowerDeficit && PowerDeficit < 0)
+    {
+        battery_state = CHARGING;
+        MainsReq = 5 * (PowerDeficit + 1);
+    }
+
+    // Offpeak
+
+    // Charge battery if small load and offpeak
+    // Charge to +2 before peak
+    // Charge to 0 after peak
+    else if ((0 <= PowerDeficit && PowerDeficit < 1) && ((hour <= 7 && battery_charge <= 2) || (hour >= 22 && battery_charge < 0)))
+    {
+        battery_state = CHARGING;
+        MainsReq = 5 * (PowerDeficit + 1);
+    }
+
+    // Charge battery, switch off lights if medium load and offpeak
+    else if ((1 <= PowerDeficit && PowerDeficit < 1.8) && ((hour <= 7 && battery_charge <= 2) || (hour >= 22 && battery_charge < 0)))
+    {
+        battery_state = CHARGING;
+        MainsReq = 5 * (PowerDeficit + 0.2);
+        housesLost = 1;
+    }
+
+    // Charge battery, switch off lifters if high load and offpeak
+    else if ((1.8 <= PowerDeficit && PowerDeficit < 3) && ((hour <= 7 && a.BatteryIntegral <= 2) || (hour >= 22 && a.BatteryIntegral < 0)))
+    {
+        battery_state = CHARGING;
+        MainsReq = 5 * (PowerDeficit + CBattery - 2);
+        housesLost = 3;
+    }
+
+    // Battery at -2
+
+    // Small load, supply with mains
+    else if ((0 <= PowerDeficit && PowerDeficit < 2) && a.BatteryIntegral <= -2)
+    {
+        CBattery = 0;
+        DBattery = 0;
+        MainsReq = 5 * (PowerDeficit);
+    }
+
+    // Medium load, switch off lights supply with mains
+    else if ((2 <= PowerDeficit && PowerDeficit < 2.8) && a.BatteryIntegral <= -2)
+    {
+        CBattery = 0;
+        DBattery = 0;
+        MainsReq = 5 * (PowerDeficit - 0.8);
+        housesLost = 1;
+    }
+
+    // High load, switch off lifters supply with mains
+    else if ((2.8 <= PowerDeficit && PowerDeficit <= 4) && a.BatteryIntegral <= -2)
+    {
+        CBattery = 0;
+        DBattery = 0;
+        MainsReq = 5 * (PowerDeficit - 2);
+        housesLost = 3;
+    }
+
+    // Battery above -2
+
+    // Small load, discharge battery and supply with mains
+    else if ((0 <= PowerDeficit && PowerDeficit < 3) && a.BatteryIntegral > -2)
+    {
+        CBattery = 0;
+        DBattery = 1;
+        MainsReq = 5 * (PowerDeficit - DBattery);
+    }
+
+    // Medium load, switch off lights discharge battery and supply with mains
+    else if ((3 <= PowerDeficit && PowerDeficit < 3.8) && a.BatteryIntegral > -2)
+    {
+        CBattery = 0;
+        DBattery = 1;
+        MainsReq = 5 * (PowerDeficit - DBattery - 0.8);
+        housesLost = 1;
+    }
+
+    // High load, switch off lifters dischage battery and supply with mains
+    else if ((3.8 <= PowerDeficit && PowerDeficit <= 4) && a.BatteryIntegral > -2)
+    {
+        CBattery = 0;
+        DBattery = 1;
+        MainsReq = 5 * (PowerDeficit - DBattery - 2);
+        housesLost = 3;
+    }
+
+    // If unsure, switch off lifters dont make battery better or worse, supply with mains
+    else
+    {
+        CBattery = 0;
+        DBattery = 0;
+        MainsReq = 10;
+        
+        if (PowerDeficit > 2) {
+            housesLost = 4;
+        }
+    }
+
+    return housesLost;
+}
 
 
 
