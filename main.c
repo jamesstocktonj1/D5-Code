0e680d00e16ff9889810d44cd24f66de1ced45a6
Butty <josephdavidbutterworth@gmail.com>
2022-03-11 18:43:47 +0000

    Algorithm implemented in main

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 #define ADC_REFRESH 100
 #define SCREEN_REFRESH 500
 
-//graphics constants
+// graphics constants
 #define HALF_WIDTH LCDWIDTH / 2
 #define QUARTER_WIDTH LCDWIDTH / 4
 
@@ -25,21 +25,18 @@
 
 #define COLUMN 100
 
-//define colours
+// define colours
 #define ORANGE 0xFD00
 
-//define colour thresholds for tricolour bar
-#define RED_THRESHOLD 870           //aprox 85%
-#define ORANGE_THRESHOLD 717        //aprox 70%
-
-
+// define colour thresholds for tricolour bar
+#define RED_THRESHOLD 870    // aprox 85%
+#define ORANGE_THRESHOLD 717 // aprox 70%
 
 // TODO
 // implement graphics for power
 // implement load names
 
-
-//graphics functions
+// graphics functions
 void draw_bar(uint16_t value, uint16_t colour);
 void draw_tricolour_bar(uint16_t value);
 void draw_signed_bar(int16_t value, uint16_t colour);
@@ -50,72 +47,64 @@ void draw_status(char *status, uint16_t colour);
 void splash_screen(void);
 void draw_screen(void);
 
-//initialise millisecond timer
+// initialise millisecond timer
 void init_timer(void);
 volatile uint32_t millis_timer;
 
-
-//runs io functions
+// runs io functions
 void read_inputs(void);
 void write_outputs(void);
 
-
-//main algorithm
+// main algorithm
 void algorithm(void);
+void load_switch(battery bat, uint8_t l1, uint8_t l2, uint8_t l3);
 
-
-//main global values
-//analog input values
+// main global values
+// analog input values
 uint16_t busbar_voltage, busbar_current;
 float wind_capacity, solar_capacity;
 
-//digital input values
+// digital input values
 uint8_t load1_call, load2_call, load3_call;
 
-//Analogue output values
+// Analogue output values
 uint16_t mains_capacity;
 
-//digital output values
+// digital output values
 uint8_t load1, load2, load3;
 battery battery_state = DISCONNECTED;
 int8_t battery_charge;
 
-
-
-ISR(TIMER0_OVF_vect) {
+ISR(TIMER0_OVF_vect)
+{
     millis_timer++;
 
-    TCNT0 = MILLIS_VALUE;   //set count register value
+    TCNT0 = MILLIS_VALUE; // set count register value
 }
 
+int main()
+{
 
-
-
-
-int main() {
-
-    //initialise screen (and set orientation)
+    // initialise screen (and set orientation)
     init_lcd();
     set_orientation(North);
     clear_screen();
 
-
-    //initialise io (io.h)
+    // initialise io (io.h)
     init_adc();
     init_pins();
 
-    //init timer
+    // init timer
     init_timer();
     sei();
 
-    //splash screen
+    // splash screen
     display_string("Hello World!");
 
     _delay_ms(2000);
 
-    
-
-    while(1) {
+    while (1)
+    {
 
         /*
         if((millis_timer % 1000) == 0) {
@@ -139,47 +128,44 @@ int main() {
             }
         }
         */
-        
 
-        if((millis_timer % SCREEN_REFRESH) == 0) {
+        if ((millis_timer % SCREEN_REFRESH) == 0)
+        {
 
             read_inputs();
-            
+
             draw_screen();
         }
 
-        //state = !state;
-
-        
+        // state = !state;
 
         _delay_ms(1);
     }
-    
+
     return 1;
 }
 
 // Main algorithm to be implemented in project
 // Average score of 79.16%
-int algorithm(struct test a, int i)
+void algorithm(void)
 {
-    double hour = millis_timer/(60*1000);
+    double hour = (double)millis_timer / (60.0 * 1000.0);
 
     // Set constants to zero
-    int housesLost = 0;
+    //int housesLost = 0;
     float MainsReq = 0;
 
     // Calculate power necessary
-    float PowerRequired = 1.2*load1_call + 2*load2_call + 0.8*load3_call;
+    float PowerRequired = 1.2 * load1_call + 2 * load2_call + 0.8 * load3_call;
     float PowerDeficit = PowerRequired - wind_capacity - solar_capacity;
 
-    float mainsSupply = (V2_2 - V2_5) - a.Wind[i] - a.PV[i];
+    //float mainsSupply = (V2_2 - V2_5) - a.Wind[i] - a.PV[i];
 
     // Charge battery if excess renewable
-    if (PowerDeficit < -1) 
+    if (PowerDeficit < -1)
     {
         battery_state = CHARGING;
         MainsReq = 0;
-
     }
 
     // Charge battery if some spare renewable and make up the difference with mains
@@ -197,214 +183,208 @@ int algorithm(struct test a, int i)
     // Charge to 0 after peak
     else if ((0 <= PowerDeficit && PowerDeficit < 1) && ((hour <= 7 && battery_charge <= 2) || (hour >= 22 && battery_charge < 0)))
     {
-        battery_state = CHARGING;
+        load_switch(CHARGING, load1_call, load2_call, load3_call);
         MainsReq = 5 * (PowerDeficit + 1);
     }
 
     // Charge battery, switch off lights if medium load and offpeak
     else if ((1 <= PowerDeficit && PowerDeficit < 1.8) && ((hour <= 7 && battery_charge <= 2) || (hour >= 22 && battery_charge < 0)))
     {
-        battery_state = CHARGING;
+        load_switch(CHARGING, load1_call, load2_call, 0);
         MainsReq = 5 * (PowerDeficit + 0.2);
-        housesLost = 1;
     }
 
     // Charge battery, switch off lifters if high load and offpeak
-    else if ((1.8 <= PowerDeficit && PowerDeficit < 3) && ((hour <= 7 && a.BatteryIntegral <= 2) || (hour >= 22 && a.BatteryIntegral < 0)))
+    else if ((1.8 <= PowerDeficit && PowerDeficit < 3) && ((hour <= 7 && battery_charge <= 2) || (hour >= 22 && battery_charge < 0)))
     {
-        battery_state = CHARGING;
-        MainsReq = 5 * (PowerDeficit + CBattery - 2);
-        housesLost = 3;
+        load_switch(CHARGING, load1_call, 0, load3_call);
+        MainsReq = 5 * (PowerDeficit - 1);
     }
 
     // Battery at -2
 
     // Small load, supply with mains
-    else if ((0 <= PowerDeficit && PowerDeficit < 2) && a.BatteryIntegral <= -2)
+    else if ((0 <= PowerDeficit && PowerDeficit < 2) && battery_charge <= -2)
     {
-        CBattery = 0;
-        DBattery = 0;
+        load_switch(DISCONNECTED, load1_call, load2_call, load3_call);
         MainsReq = 5 * (PowerDeficit);
     }
 
     // Medium load, switch off lights supply with mains
-    else if ((2 <= PowerDeficit && PowerDeficit < 2.8) && a.BatteryIntegral <= -2)
+    else if ((2 <= PowerDeficit && PowerDeficit < 2.8) && battery_charge <= -2)
     {
-        CBattery = 0;
-        DBattery = 0;
+        load_switch(DISCONNECTED, load1_call, load2_call, 0);
         MainsReq = 5 * (PowerDeficit - 0.8);
-        housesLost = 1;
     }
 
     // High load, switch off lifters supply with mains
-    else if ((2.8 <= PowerDeficit && PowerDeficit <= 4) && a.BatteryIntegral <= -2)
+    else if ((2.8 <= PowerDeficit && PowerDeficit <= 4) && battery_charge <= -2)
     {
-        CBattery = 0;
-        DBattery = 0;
+        load_switch(DISCONNECTED, load1_call, 0, load3_call);
         MainsReq = 5 * (PowerDeficit - 2);
-        housesLost = 3;
     }
 
     // Battery above -2
 
     // Small load, discharge battery and supply with mains
-    else if ((0 <= PowerDeficit && PowerDeficit < 3) && a.BatteryIntegral > -2)
+    else if ((0 <= PowerDeficit && PowerDeficit < 3) && battery_charge > -2)
     {
-        CBattery = 0;
-        DBattery = 1;
-        MainsReq = 5 * (PowerDeficit - DBattery);
+        load_switch(DISCHARGING, load1_call, load2_call, load3_call);
+        MainsReq = 5 * (PowerDeficit - 1);
     }
 
     // Medium load, switch off lights discharge battery and supply with mains
-    else if ((3 <= PowerDeficit && PowerDeficit < 3.8) && a.BatteryIntegral > -2)
+    else if ((3 <= PowerDeficit && PowerDeficit < 3.8) && battery_charge > -2)
     {
-        CBattery = 0;
-        DBattery = 1;
-        MainsReq = 5 * (PowerDeficit - DBattery - 0.8);
-        housesLost = 1;
+        load_switch(DISCHARGING, load1_call, load2_call, 0);
+        MainsReq = 5 * (PowerDeficit - 1.8);
     }
 
     // High load, switch off lifters dischage battery and supply with mains
-    else if ((3.8 <= PowerDeficit && PowerDeficit <= 4) && a.BatteryIntegral > -2)
+    else if ((3.8 <= PowerDeficit && PowerDeficit <= 4) && battery_charge > -2)
     {
-        CBattery = 0;
-        DBattery = 1;
-        MainsReq = 5 * (PowerDeficit - DBattery - 2);
-        housesLost = 3;
+        load_switch(DISCHARGING, load1_call, 0, load3_call);
+        MainsReq = 5 * (PowerDeficit - 3);
     }
 
     // If unsure, switch off lifters dont make battery better or worse, supply with mains
     else
     {
-        CBattery = 0;
-        DBattery = 0;
+        load_switch(DISCONNECTED, load1_call, 0, load3_call);
         MainsReq = 10;
-        
-        if (PowerDeficit > 2) {
-            housesLost = 4;
-        }
     }
-
-    return housesLost;
 }
 
+// Set loads to appropriate values
+void load_switch(battery bat, uint8_t l1, uint8_t l2, uint8_t l3)
+{
+    battery_state = bat;
+    load1 = l1;
+    load2 = l2;
+    load3 = l3;
+}
 
-
-void init_timer() {
-
-    //set to normal mode, no output
+void init_timer()
+{
+    // set to normal mode, no output
     TCCR0A = 0x00;
 
-    //clear register
+    // clear register
     TCCR0B = 0x00;
 
-    TCCR0B |= _BV(CS00) | _BV(CS01);        //set 64 clock divide
+    TCCR0B |= _BV(CS00) | _BV(CS01); // set 64 clock divide
 
-    TIMSK0 |= _BV(TOIE0);                    //enable overflow interrupt
-    
-    TCNT0 = MILLIS_VALUE;                   //set count register value
+    TIMSK0 |= _BV(TOIE0); // enable overflow interrupt
 
-    //set timer variable to 0
+    TCNT0 = MILLIS_VALUE; // set count register value
+
+    // set timer variable to 0
     millis_timer = 0;
 }
 
+void draw_bar(uint16_t value, uint16_t colour)
+{
 
-
-void draw_bar(uint16_t value, uint16_t colour) {
-
-    //convert 0-1024 to 0-128
+    // convert 0-1024 to 0-128
     value = value / 8;
 
-    //setup rectangle
+    // setup rectangle
     rectangle bar;
     bar.top = display.y;
     bar.left = display.x;
     bar.bottom = display.y + 7;
     bar.right = display.x + value;
-    
+
     fill_rectangle(bar, colour);
 }
 
-void draw_tricolour_bar(uint16_t value) {
+void draw_tricolour_bar(uint16_t value)
+{
 
     uint16_t colour = GREEN;
 
-    if(value > RED_THRESHOLD) {
+    if (value > RED_THRESHOLD)
+    {
         colour = RED;
     }
-    else if(value > ORANGE_THRESHOLD) {
+    else if (value > ORANGE_THRESHOLD)
+    {
         colour = ORANGE;
     }
 
     draw_bar(value, colour);
 }
 
-void draw_signed_bar(int16_t value, uint16_t colour) {
+void draw_signed_bar(int16_t value, uint16_t colour)
+{
 
-    //converty +-512 to +-64
+    // converty +-512 to +-64
     value = value / 4;
 
-    //setup rectangle
+    // setup rectangle
     rectangle bar;
     bar.top = display.y;
     bar.bottom = display.y + 7;
 
-    //sets the l/r coords depending on whether the value is positive or negative
-    bar.left    = (value < 0) ? display.x + 64 + value : display.x + 64;
-    bar.right   = (value < 0) ? display.x + 64 : display.x + 64 + value;
+    // sets the l/r coords depending on whether the value is positive or negative
+    bar.left = (value < 0) ? display.x + 64 + value : display.x + 64;
+    bar.right = (value < 0) ? display.x + 64 : display.x + 64 + value;
 
     fill_rectangle(bar, colour);
-
 }
 
-void draw_battery_state(battery state) {
+void draw_battery_state(battery state)
+{
 
-    //create indicator square of size 7x7 (pixel height square)
+    // create indicator square of size 7x7 (pixel height square)
     rectangle ind;
     ind.top = display.y;
     ind.left = display.x;
     ind.bottom = display.y + 7;
     ind.right = display.x + 7;
 
-    //color variable
+    // color variable
     uint16_t col;
 
-    switch(state) {
+    switch (state)
+    {
 
-        case CHARGING:
-            col = GREEN;
-            break;
+    case CHARGING:
+        col = GREEN;
+        break;
 
-        case DISCHARGING:
-            col = RED;
-            break;
+    case DISCHARGING:
+        col = RED;
+        break;
 
-        default:
-            col = YELLOW;
-            break;
+    default:
+        col = YELLOW;
+        break;
     }
 
-    //draw indicator
+    // draw indicator
     fill_rectangle(ind, col);
 }
 
-void draw_indicator(uint8_t state) {
+void draw_indicator(uint8_t state)
+{
 
-    //create indicator square of size 7x7 (pixel height square)
+    // create indicator square of size 7x7 (pixel height square)
     rectangle ind;
     ind.top = display.y;
     ind.left = display.x;
     ind.bottom = display.y + 7;
     ind.right = display.x + 7;
 
-    //color variable (green or read)
+    // color variable (green or read)
     uint16_t col = (state) ? GREEN : RED;
 
-    //draw indicator
+    // draw indicator
     fill_rectangle(ind, col);
 }
 
-void draw_status(char *status, uint16_t colour) {
+void draw_status(char *status, uint16_t colour)
+{
 
     rectangle stat;
     stat.top = 270;
@@ -419,17 +399,13 @@ void draw_status(char *status, uint16_t colour) {
     display_string(status);
 }
 
+void draw_screen()
+{
 
-
-
-
-void draw_screen() {
-
-    //clear changing area of the screen
+    // clear changing area of the screen
     clear_screen();
 
-
-    //display wind capacity
+    // display wind capacity
     display.y = 20;
 
     display.x = LINDENT;
@@ -438,8 +414,7 @@ void draw_screen() {
     display.x = COLUMN;
     draw_bar(wind_capacity, GREEN);
 
-
-    //display solar capacity
+    // display solar capacity
     display.y = 40;
 
     display.x = LINDENT;
@@ -448,8 +423,7 @@ void draw_screen() {
     display.x = COLUMN;
     draw_bar(solar_capacity, GREEN);
 
-
-    //display mains capacity
+    // display mains capacity
     display.y = 60;
 
     display.x = LINDENT;
@@ -458,8 +432,7 @@ void draw_screen() {
     display.x = COLUMN;
     draw_bar(mains_capacity, GREEN);
 
-
-    //display battery status
+    // display battery status
     display.y = 80;
 
     display.x = LINDENT;
@@ -471,8 +444,7 @@ void draw_screen() {
     display.x = COLUMN + 15;
     draw_bar(battery_charge, GREEN);
 
-
-    //display call 1
+    // display call 1
     display.y = 120;
 
     display.x = LINDENT;
@@ -484,7 +456,7 @@ void draw_screen() {
     display.x = COLUMN * 1.5;
     draw_indicator(load1);
 
-    //display call 2
+    // display call 2
     display.y = 140;
 
     display.x = LINDENT;
@@ -496,7 +468,7 @@ void draw_screen() {
     display.x = COLUMN * 1.5;
     draw_indicator(load2);
 
-    //display call 3
+    // display call 3
     display.y = 160;
 
     display.x = LINDENT;
@@ -508,9 +480,7 @@ void draw_screen() {
     display.x = COLUMN * 1.5;
     draw_indicator(load3);
 
-
-
-    //display busbar voltage
+    // display busbar voltage
     display.y = 200;
 
     display.x = LINDENT;
@@ -519,8 +489,7 @@ void draw_screen() {
     display.x = COLUMN;
     draw_bar(busbar_voltage, GREEN);
 
-
-    //display busbar current
+    // display busbar current
     display.y = 220;
 
     display.x = LINDENT;
@@ -529,21 +498,19 @@ void draw_screen() {
     display.x = COLUMN;
     draw_bar(busbar_current, GREEN);
 
-    
-    //display busbar power
+    // display busbar power
     display.y = 240;
 
     display.x = LINDENT;
     display_string("Busbar Power:");
-    
+
     uint16_t busbar_power = 0;
-    busbar_power = (busbar_current * busbar_power) / 1024;
+    busbar_power = (busbar_current * busbar_voltage) / 1024;
 
     display.x = COLUMN;
     draw_bar(busbar_power, GREEN);
 
-
-    //draw status box
+    // draw status box
     draw_status("Good", GREEN);
 }
 
@@ -628,11 +595,10 @@ void draw_screen() {
     display_string(num);
 }*/
 
+void read_inputs(void)
+{
 
-
-void read_inputs(void) {
-
-    //read analog values
+    // read analog values
     mains_capacity = get_mains_capacity();
 
     busbar_voltage = get_busbar_voltage();
@@ -641,18 +607,16 @@ void read_inputs(void) {
     wind_capacity = get_wind_capacity();
     solar_capacity = get_solar_capacity();
 
-
-    //read digital values
+    // read digital values
     load1_call = get_load1();
     load2_call = get_load2();
     load3_call = get_load3();
 }
 
+void write_outputs()
+{
 
-
-void write_outputs() {
-
-    //set output values
+    // set output values
     set_load1(load1);
     set_load2(load2);
     set_load3(load3);
@@ -660,7 +624,6 @@ void write_outputs() {
     set_battery_state(battery_state);
 }
 
-
-void splash_screen() {
-
+void splash_screen()
+{
 }
